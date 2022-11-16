0810b32748a856d15d2ffcebe20b3d5a7de5e3ac
David Linkowski <david.linkowski@gmail.com>
2022-11-16 23:56:18 +0000

    Iteration 4 update 1

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+
+
 RSpec.describe Project, type: :model do
+
+  context "scopes tests" do
+      let(:params) { { title: "Title", description: "some description"  } }
+      before (:each) do
+        Project.create(params)
+        Project.create(params)
+        Project.create(params)
+      end
+
+      it "should return all projects" do
+        expect(Project.count).to eq(3)
+      end
+    end
+  end
+
   context "validations tests" do
     it "ensures the description is present" do
       project = Project.new(description: "Content of the description")
