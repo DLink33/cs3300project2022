950b9d20b1f43e626d968b648a1502aca01a5a9f
David Linkowski <david.linkowski@gmail.com>
2022-12-10 22:02:09 +0000

    Iteration 6: Log In sceen html finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  root to: "home#index"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
